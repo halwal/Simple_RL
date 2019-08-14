48e8b151c490240e72163f7fcc679ff4f0c94b44
Halszka Walkiewicz <halwsk@gmail.com>
2019-08-14 11:42:39 +0200

    Furthered tutorial

---
+++ map_objects/game_map.py
@@ -0,0 +1,38 @@
+from map_objects.tile import Tile
+from map_objects.rectangle import Rect
+
+class GameMap:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tiles = self.initialize_tiles()
+
+    def initialize_tiles(self):
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
+
+        return tiles
+
+    def make_map(self, max_rooms, room_min_size, room_max_size, ):
+
+    def create_room(self, room):
+        # go through tiles in rectangle, make them passable
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1+1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1,x2), max(x1,x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1,y2), max(y1,y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def is_blocked(self, x, y):
+        if self.tiles[x][y].blocked:
+            return True
+        return False
+
