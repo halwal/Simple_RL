817a0ab9d2736cb6953ae19c1d174e8bdcc2c8c9
Halszka Walkiewicz <halwsk@gmail.com>
2019-08-20 14:13:10 +0200

    Part 6 of tutorial finished!

--- engine.py
+++ engine.py
@@ -1,10 +1,12 @@
 import tcod as libtcod
 from game_states import GameStates
 from input_handlers import handle_keys
+from components.fighter import Fighter
 from entity import Entity, get_blocking_entities_at_location
-from render_functions import clear_all, render_all
+from render_functions import clear_all, render_all, RenderOrder
 from map_objects.game_map import GameMap
 from fov_functions import initialize_fov, recompute_fov
+from death_functions import kill_monster, kill_player
 
 def main():
     screen_width = 80
@@ -29,7 +31,8 @@ def main():
         'light_ground': libtcod.sepia
      }
 
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component)
     entities = [player]
 
 
@@ -56,7 +59,7 @@ def main():
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
 
         fov_recompute = False
 
@@ -70,6 +73,8 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        player_turn_results = []
+
         if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
 
@@ -79,7 +84,8 @@ def main():
             if not game_map.is_blocked(destination_x, destination_y):
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
                 if target:
-                    print('You kick the ' + target.name + ' in the shins, much to its annoyance!')
+                    attack_results = player.fighter.attack(target)
+                    player_turn_results.extend(attack_results)
                 else:
                     player.move(dx, dy)
 
@@ -91,12 +97,48 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
+        for player_turn_result in player_turn_results:
+            message = player_turn_result.get('message')
+            dead_entity = player_turn_result.get('dead')
+
+            if message:
+                print(message)
+
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+
+                print(message)
+
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print('The ' + entity.name + ' ponders the meaning of its existence!')
+                if entity.ai:
+                    enemy_turn_results = entity.ai.take_turn(player, fov_map, game_map, entities)
 
-            game_state = GameStates.PLAYERS_TURN
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+
+                        if message:
+                            print(message)
+
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+
+                            print(message)
+
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+
+                    if game_state == GameStates.PLAYER_DEAD:
+                        break
+            else:
+                game_state = GameStates.PLAYERS_TURN
 
 
 
--- input_handlers.py
+++ input_handlers.py
@@ -1,22 +1,23 @@
 import tcod as libtcod
 
 def handle_keys(key):
-    #movement keys
-    if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
+    key_char = chr(key.c)
+    # movement keys
+    if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8 or key_char == 'k':
         return {'move' : (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
+    elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2 or key_char == 'j':
         return {'move' : (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
+    elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4 or key_char == 'h':
         return {'move' : (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
+    elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6 or key_char == 'l':
         return {'move' : (1, 0)}
-    elif (key.vk == libtcod.KEY_UP and key.vk == libtcod.KEY_RIGHT) or key.vk == libtcod.KEY_KP9:
+    elif (key.vk == libtcod.KEY_UP and key.vk == libtcod.KEY_RIGHT) or key.vk == libtcod.KEY_KP9 or key_char == 'u':
         return {'move' : (1, -1)}
-    elif (key.vk == libtcod.KEY_DOWN and key.vk == libtcod.KEY_RIGHT) or key.vk == libtcod.KEY_KP3:
+    elif (key.vk == libtcod.KEY_DOWN and key.vk == libtcod.KEY_RIGHT) or key.vk == libtcod.KEY_KP3 or key_char == 'n':
         return {'move' : (1, 1)}
-    elif (key.vk == libtcod.KEY_UP and key.vk == libtcod.KEY_LEFT) or key.vk == libtcod.KEY_KP7:
+    elif (key.vk == libtcod.KEY_UP and key.vk == libtcod.KEY_LEFT) or key.vk == libtcod.KEY_KP7 or key_char == 'y':
         return {'move' : (-1, -1)}
-    elif (key.vk == libtcod.KEY_DOWN and key.vk == libtcod.KEY_LEFT) or key.vk == libtcod.KEY_KP1:
+    elif (key.vk == libtcod.KEY_DOWN and key.vk == libtcod.KEY_LEFT) or key.vk == libtcod.KEY_KP1 or key_char == 'b':
         return {'move' : (-1, 1)}
 
 
